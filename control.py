27ff5e300a71215c916f8de29d2fd84b4353a462
Christina Nuss-Brill <ge64lav@mytum.de>
2021-11-04 19:07:03 +0100

    Update control.py

--- control.py
+++ control.py
@@ -110,12 +110,11 @@ class PedestrianController:
                 a.is_inside(p)
             # loops pedestrians to the left if density is being calculated used for task 3 test 2
             if self.with_density and p.cell.loc[0] in [t.cell.loc[0]-2 for t in self.targets]:
-                self.pedestrians.append(Pedestrian(self.field.cells[1, p.cell.loc[1]], p.speed, p.steps_left, p.identity))
+                self.pedestrians.append(Pedestrian(self.field.cells[1, p.cell.loc[1]], p.speed, p.steps_left, p identity))
                 remove_pedestrians.append(p)
-<<<<<<< HEAD
+
              #Find and move to the neighbor with the lowest cost function
-=======
->>>>>>> 06d76e98b7b824bae9cc9ad397bc7bc754c0e8bd
+
             if not p.cell in [t.cell for t in self.targets]:
                 optimal_neighbor = self.find_optimal_neighbor(p)
                 p.move_in_time(optimal_neighbor)
